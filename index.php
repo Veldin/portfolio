4458553b1493586160d12b02142b7968573e69bb
Veldin <amr.jonkman@gmail.com>
2016-12-13 13:13:26 +0100

    Standart page layout.
    Standart page layout toegevoegd.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	
 	$core = new Core;
 	$pages = new Pages;
-	$dbc = '';
+	$dbc = $core->dbc();
 ?>
 <html>
 	<head>
@@ -16,11 +16,26 @@
 		<meta http-equiv="content-type" content="text/html;charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="style.css" rel="stylesheet" type="text/css">
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
 	<body>
 		<?php 
-			echo '<div id="container">';
-				$core->load();
+			echo '<div id="headerOuter">';
+				echo '<div id="headerInner">';
+					$pages->header();
+				echo '</div>';
+			echo '</div>';
+		
+			echo '<div id="containerOuter">';
+					$core->load();
+			echo '</div>';
+			
+			echo '<div id="footerOuter">';
+				echo '<div id="footerInner">';
+					$pages->footer();
+				echo '</div>';
 			echo '</div>';
 		?>
 	</body>
--- pages.php
+++ pages.php
@@ -4,8 +4,92 @@ class Pages {
 	//Hoofd pagina
 	function home(){
 		global $core;
+
+	}
+	
+	function header(){
+		echo 'HEADER';
+	}
+	
+	function footer(){
+		echo 'FOOTER';
+	}
+	
+	//Test voor de database.
+	//Toon alle info in de database.
+	function testdb(){
+		global $dbc;
+
+		//Test 
+		$sth = $dbc->prepare('show tables');
+		$sth->execute();
 		
-		echo 'sdfdsf';
+		$all = $sth->fetchAll(PDO::FETCH_OBJ);
+		
+		foreach ($all as $value) {
+			echo '<hr>';
+			echo '<h2>'.$value->Tables_in_md253219db370063.'</h2>';
+			echo '<br>';
+			
+			echo '<b>Describe</b><br>';
+			$describe = $dbc->prepare('describe '.$value->Tables_in_md253219db370063);
+			$describe->execute();
+			$describe = $describe->fetchAll(PDO::FETCH_OBJ);
+			foreach ($describe as $describe_value) {
+				print_r($describe_value);
+				echo '<br>';
+			}
+			
+			echo '<b>Select</b><br>';
+			$display = $dbc->prepare('select * from '.$value->Tables_in_md253219db370063);
+			$display->execute();
+			$display = $display->fetchAll(PDO::FETCH_BOTH);
+			foreach ($display as $value) {
+				echo '<br>';
+				print_r($value);
+				echo '<br>';
+			}
+			
+		}
+	}
+	
+	//404
+	function notfound(){
+	
+		echo '404';
+	}
+	
+	//Laten zien van portfolio
+	function portfolio(){
+		global $dbc;
+	
+		echo '<div id="containerOuter">';
+			echo '<div id="containerInner">';
+			if(isset($_GET["u"])){
+				$user = htmlspecialchars($_GET["u"]);
+				echo $user;
+				
+				$portfolio = $dbc->prepare('SELECT * FROM `portfolio` WHERE `url` = "'.$user.'"');
+				$portfolio->execute();
+				$portfolio = $portfolio->fetchAll(PDO::FETCH_BOTH);
+				
+				if(!empty($portfolio)){
+					echo 'User exists';
+				}else{
+					echo 'User does not exists.';
+				}
+			}else{
+				echo 'Geen gebruiker geselecteerd.';
+			}
+			echo '</div>';
+		echo '</div>';
+		
+		echo '<div id="containerOuter">';
+			echo '<div id="containerInner">';
+				echo 'Andere content';
+			echo '</div>';
+		echo '</div>';
+	
 	}
 }
 ?>
